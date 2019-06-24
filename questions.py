b569d644575a339f7e8207daf162d9e7453365a2
Eliska Jegrova <jegrovaeliska@gmail.com>
2019-06-24 17:18:51 +0200

    Add questions for the quiz

---
+++ questions.py
@@ -0,0 +1,334 @@
+question_list = {
+    "q_0":
+
+    {"text": "Kdo neudělal chybu? (Video 5.turnaj 3:28) (5)",
+     "correct_answer": 0,
+     "options": [
+         ["Dany", 0],
+         ["Kačaba", 5],
+         ["Žanet", 0],
+         ["Klárka", 0],
+     ]
+     },
+    "q_1":
+
+    {"text": "Kdo při ztrátě míčku neměl hráčku? (Video 5.turnaj 0:19)  (5) ",
+     "correct_answer": 0,
+     "options": [
+         ["Kačaba", 5],
+         ["Žanet", 0],
+         ["Klárka", 0],
+         ["Zdenka", 0],
+     ]
+     },
+    "q_2":
+
+    {"text": "Kdo u rozehrávky neměl správné postavení? (Video 5.turnaj 0:53)  (5)",
+     "correct_answer": 0,
+     "options": [
+         ["Peťa", 5],
+         ["Barča", 0],
+         ["Zorka", 0],
+         ["Elza", 0],
+     ]
+     },
+    "q_3":
+
+    {"text": "Kdo u rozehrávky vůbec neměl správné postavení? (Video 5.turnaj 1:06)  (5)",
+     "correct_answer": 0,
+     "options": [
+         ["Kačaba", 0],
+         ["Dany", 0],
+         ["Žanet", 5],
+         ["Zdenka", 0],
+     ]
+     },
+    "q_4":
+
+    {"text": "Co bylo špatně? (Video 5.turnaj 1:15)  (5)",
+     "correct_answer": 0,
+     "options": [
+         ["Dany má svoji hráčku daleko", 0],
+         ["Žanet nebrání správnou hráčku", 0],
+         ["Špatně rozebrané hráčky na konci videa ", 5],
+         ["žádná z uvedených", 0],
+     ]
+     },
+    "q_5":
+
+    {"text": "Jaktože se hráčka dostala sama do našeho pravého rohu? (Video 5.turnaj 1:29)  (5)",
+     "correct_answer": 0,
+     "options": [
+         ["Barča brání u brankoviště", 5],
+         ["Klárka brání nahrávku", 0],
+         ["Žanet se tam měla stáhnout", 0],
+         ["Kačaba nebrání správnou hráčku", 0],
+     ]
+     },
+    "q_6":
+
+    {"text": "Co je na rozehrávce správně? (Video 5.turnaj 1:56)  (5)",
+     "correct_answer": 0,
+     "options": [
+         ["pravý útočník je ve správném postavení", 0],
+         ["centr je ve správném postavení", 0],
+         ["obránce je u rozehrávky", 5],
+         ["druhý obránce po obdržení míčku správně odehrává", 0],
+     ]
+     },
+    "q_7":
+
+    {"text": "Co vedlo k tomu, že se naše hráčky ocitly ve špatném postavení při bránění? (Video 5.turnaj 2:09)  (5,5)",
+     "correct_answer": 0,
+     "options": [
+         ["klárka propadla v souboji", 0],
+         ["barča měla mít hráčku na středu hřiště", 5],
+         ["jíťa měla mít hráčku na středu hříště", 0],
+         ["zorka se nestáhla k volné hráčce před ní", 5],
+     ]
+     },
+    "q_8":
+
+    {"text": "Kdo byl na rozehrávce opravdu špatně? (Video 5.turnaj 2:21)  (5)",
+     "correct_answer": 0,
+     "options": [
+         ["Klárka", 5],
+         ["Žanet", 0],
+         ["Dany", 0],
+         ["Kačaba", 0],
+     ]
+     },
+    "q_9":
+
+    {"text": "Copak dělá Zorka špatně? (Video 5.turnaj 2:34)  (5)",
+     "correct_answer": 0,
+     "options": [
+         ["Běží s ním kolmo na mantinel", 5],
+         ["Zpracovává míček zády ke hře", 0],
+         ["Nechá si u mantinelu míček vypíchnout", 0],
+         ["Zorka udělala všechno správně - žádná z ostatních odpovědí není správná.", 0],
+     ]
+     },
+    "q_10":
+
+    {"text": "Pokud padne gól při 5 minutovém vyloučení, vrací se hráčka zpět do hry?  (5)",
+     "correct_answer": 0,
+     "options": [
+         ["Ne", 5],
+         ["Ano", 0],
+         ["Nevím", 0],
+         ["Je mi to fuk", 0],
+     ]
+     },
+    "q_11":
+
+    {"text": "Pokud dojde k 2 min vyloučení hráčky A v čase 1:50 a hráčky B ze stejného týmů se stejným trestem v čase 2:30, kdy vyprší tresty hráček?  (5)",
+     "correct_answer": 0,
+     "options": [
+         ["A - 3:50, B - 4:30", 5],
+         ["A - 3:50, B - 5:50", 0],
+         ["A - 5:50, B - 4:30", 0],
+         ["Pokud padne gól, pak se vrací obě hráčky z trestné lavice.", 0],
+     ]
+     },
+    "q_12":
+
+    {"text": "Kdo má správné rozestavení na obrázku? (5)",
+     "correct_answer": 0,
+     "options": [
+         ["Nikdo", 5],
+         ["Oba týmy", 0],
+         ["Béčko", 0],
+         ["Céčko", 0],
+     ]
+     },
+    "q_13":
+
+    {"text": "Může brankář vyskočit a vyrazit míček? (nedotýká se tedy žádnou části těla plochy velkého brankoviště) Nebo to může jen Kačaba za 2min ?  (5)",
+     "correct_answer": 0,
+     "options": [
+         ["Ano", 5],
+         ["Ne", 0],
+         ["Může si 'povyskočit', tedy dotýkat se alespoň špičkou nohy povrchu.", 0],
+         ["Je mi to fuk", 0],
+     ]
+     },
+    "q_14":
+
+    {"text": "Ze které z této pozic je nejvhodnější střílet? (za předpokladu, že je zde i bránicí družstvo)  (5)",
+     "correct_answer": 0,
+     "options": [
+         ["Běží s ním kolmo na mantinel", 5],
+         ["Zpracovává míček zády ke hře", 0],
+         ["Nechá si u mantinelu míček vypíchnout", 0],
+         ["Zorka udělala všechno správně - žádná z ostatních odpovědí není správná.", 0],
+     ]
+     },
+    "q_15":
+
+    {"text": "Co chybělo při této útočné akci? (Video 1)  (5)",
+     "correct_answer": 0,
+     "options": [
+         ["clonění před bránou", 5],
+         ["náběh obránce na střelu", 0],
+         ["náběh pravého křídla na střelu", 0],
+         ["z této pozice se nemělo střílet", 0],
+     ]
+     },
+    "q_16":
+
+    {"text": "Co způsobilo jejich gólovou situaci? (Video 2)  (5)",
+     "correct_answer": 0,
+     "options": [
+         ["špatné střídání obránců", 5],
+         ["nedostatečný důraz Kačaby ", 0],
+         ["špatné postavení Žanet", 0],
+         ["propadení Káti", 0],
+     ]
+     },
+    "q_17":
+
+    {"text": "Co je na této situaci nevhodné? (Video 3)  (5)",
+     "correct_answer": 0,
+     "options": [
+         ["střelba Klárky", 5],
+         ["náběh Zorky na střed ", 0],
+         ["náběh Elzy na mantinel", 0],
+         ["Bedla neklečí", 0],
+     ]
+     },
+    "q_18":
+
+    {"text": "Po zmocnění míčku Žanet (1:28), které tvrzení není pravdivé? (Video 4)  (5)",
+     "correct_answer": 0,
+     "options": [
+         ["Kačaba střílela ve správnou chvíli.", 5],
+         ["Žanet by měla běžet do středu hřiště.", 0],
+         ["Šupina je ve správné pozici.", 0],
+         ["Janča si hlídá hráčku a má to tak být.", 0],
+     ]
+     },
+    "q_19":
+
+    {"text": "Co nezpůsobilo gol? (Video 5)  (5)",
+     "correct_answer": 0,
+     "options": [
+         ["daleké postavení Šupiny ve chvíli kdy má Janča míček", 5],
+         ["nevhodná nahrávka Janči", 0],
+         ["propad celého útoku", 0],
+         ["nepokrytí střel", 0],
+     ]
+     },
+    "q_20":
+
+    {"text": "Jaké je postavení našich hráček u rozehrávky? (Video 6)  (5)",
+     "correct_answer": 0,
+     "options": [
+         ["správné", 5],
+         ["Kačaba je špatně", 0],
+         ["Dany je špatně", 0],
+         ["Zorka je špatně", 0],
+     ]
+     },
+    "q_21":
+
+    {"text": "Kdo neudělal chybu?  (Video 7)  (5)",
+     "correct_answer": 0,
+     "options": [
+         ["Žanet a Káťa", 5],
+         ["Janča a Klárka", 0],
+         ["Káťa a Šupina", 0],
+         ["Žanet a Šupina", 0],
+     ]
+     },
+    "q_22":
+
+    {"text": "Povedlo se přebrání hráček?  (Video 8)  (5)",
+     "correct_answer": 0,
+     "options": [
+         ["Bylo to příšerné ", 5],
+         ["ano ", 0],
+         ["ne ", 0],
+         ["Bylo to uplně v klidu", 0],
+     ]
+     },
+    "q_23":
+
+    {"text": "Byly naše hráčky správně postavené na rozehrávce?  (Video 9)  (5)",
+     "correct_answer": 0,
+     "options": [
+         ["ne", 5],
+         ["ano", 0],
+         ["nevím", 0],
+         ["na tohle odpovídat nebudu.", 0],
+     ]
+     },
+    "q_24":
+
+    {"text": "Balón poskakuje ve velkém brankovišti. Brankář domácích kopne do balónu přímo do běhu spoluhráči a ten balón na vlastní polovině hřiště zpracuje. Co bude následovat? (5)",
+     "correct_answer": 0,
+     "options": [
+         ["Hra bude pokračovat.", 5],
+         ["Volný úder pro hostující družstvo 3,5m od malého brankoviště.", 0],
+         ["Volný úder pro hostující družstvo na místě přijetí přihrávky.", 0],
+         ["Vhazování na nejbližším; bodě pro vhazování. ", 0],
+     ]
+     },
+    "q_25":
+
+    {"text": "Je možno přímo z vhazování vstřelit branku?  (5)",
+     "correct_answer": 0,
+     "options": [
+         ["Ano.", 5],
+         ["Ne.", 0],
+         ["Závisí na posouzení rozhodčího.", 0],
+         ["Ano, ale pouze na útočné polovině.", 0],
+     ]
+     },
+    "q_26":
+
+    {"text": "Kam by mělo nabíhat levé křídlo?  (5,2)",
+     "correct_answer": 0,
+     "options": [
+         ["K pravému mantinelu na soupeřově straně hřiště", 5],
+         ["K levému mantinelu na naší půlce hřiště", 0],
+         ["Na střed za polovinu hřiště", 0],
+         ["Na levou polovinu soupeřovy strany hřiště", 2],
+     ]
+     },
+    "q_27":
+
+    {"text": "Kam by mělo nabíhat levé křídlo? (5,2)",
+     "correct_answer": 0,
+     "options": [
+         ["za branku", 5],
+         ["před brankku", 0],
+         ["do levého roku", 2],
+         ["na hokejový pravý kruh", 0],
+     ]
+     },
+    "q_28":
+
+    {"text": "Na kterém místě by měl být pravý obránce, když má míček protihráčka v rohu? (5)",
+     "correct_answer": 0,
+     "options": [
+         ["C", 5],
+         ["A", 0],
+         ["B", 0],
+         ["D", 0],
+     ]
+     },
+    "q_29":
+
+    {"text": "Kdo neudělal chybu? (Video 115 8.turnaj)  (5)",
+     "correct_answer": 0,
+     "options": [
+         ["Jíťa a Žanet", 5],
+         ["Jíťa a Kačaba", 0],
+         ["Jíťa a Lucka", 0],
+         ["Jíťa a Barča", 0],
+     ]
+     }
+
+
+}
